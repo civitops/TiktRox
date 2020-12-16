78df97eba594d7724037bf5e76bb2e97ebb5d73f
RexterR <sumandas.workplace@gmail.com>
2020-12-16 10:16:55 +0530

    common: adding currentuser middleware

--- common/src/index.ts
+++ common/src/index.ts
@@ -5,3 +5,5 @@ export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
+
+export * from './middlewares/current-user';
